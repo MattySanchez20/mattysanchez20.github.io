95635d7328ec7b18418b1b4155494db92102c58b
MattySanchez20 <73239506+MattySanchez20@users.noreply.github.com>
2025-08-08 09:18:33 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-@@ .. @@
-  const projects = [
+const projects = [
     {
       title: "Dogfight Simulator",
       description: "Built a really fun and simple fighter jet simulator (based on geometry and statistics) where the dogfight is logged in a file for memorabilia",
       tech: ["Python", "FastAPI", "MongoDB", "OOP"],
--      status: "Personal"
       status: "Personal",
       sourceUrl: "https://github.com/mattysanchez20/dogfight-simulator"
-+      status: "Personal",
-+      sourceUrl: "https://github.com/mattysanchez20/dogfight-simulator"
     },
     {
       title: "Solar Panel CNN",
       description: "Developed a CNN model to classify an image of a solar panel as clean or dirty",
       tech: ["Python", "TensorFlow", "CNN", "Optimisation Algorithms"],
--      status: "WIP"
-+      status: "WIP",
-+      sourceUrl: "https://github.com/mattysanchez20/solar-panel-cnn"
+      status: "WIP",
+      sourceUrl: "https://github.com/mattysanchez20/solar-panel-cnn"
     },
     {
       title: "Work Projects",
       description: "Ask me about them!",
       tech: ["Mystery"],
--      status: "Production"
-+      status: "Production",
-+      sourceUrl: null
+      status: "Production",
+      sourceUrl: null
     }
   ];
